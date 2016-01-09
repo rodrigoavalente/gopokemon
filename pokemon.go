74d3eceaa6cceea26b5259949a631628b705a21a
Rodrigo Valente <rodrigoavrav@gmail.com>
2016-01-09 18:11:22 -0300

    Added function query

--- pokemon.go
+++ pokemon.go
@@ -1,10 +1,27 @@
 package gopokemon
 
 import (
+	"encoding/json"
 	"net/http"
 )
 
 type PokemonData struct {
 	Name       string `json:"name"`
 	NationalID int    `json:"national_id"`
+}
+
+func QueryPokemon(pokemon_id string) (PokemonData, error) {
+	response, err := http.Get("http://pokeapi.co/api/v1/pokemon/" + pokemon_id + "/")
+
+	if err != nil {
+		return PokemonData{}, nil
+	}
+
+	defer response.Body.Close()
+	var pokemon PokemonData
+	if err := json.NewDecoder(response.Body).Decode(&pokemon); err != nil {
+		return PokemonData{}, err
+	}
+
+	return pokemon, nil
 }
